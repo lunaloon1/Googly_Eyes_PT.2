572702aa8a862b5cc491216ce8533881d8093125
cameronallison001 <allisoncameron05@gmail.com>
2024-11-13 23:43:08 -0600

    Update CODE_AND_FAB_FINALPROJECT_CODE_copy_20241105132657.ino

--- CODE_AND_FAB_FINALPROJECT_CODE_copy_20241105132657.ino
+++ CODE_AND_FAB_FINALPROJECT_CODE_copy_20241105132657.ino
@@ -1,9 +1,36 @@
+onst int ledPin = 11; 
+const int servoPin1 = 4; 
+const int buttonAPin = 13; 
+int buttonAState = 0; 
+int lastButtonAState = 0; 
+bool A = false; 
+
+#include <Servo.h> 
+Servo myservo; 
+const int buttonAPin = 13; 
+
 void setup() {
-  // put your setup code here, to run once:
-// GOOGLY EYES
+  myservo.attach(4)
+  pinMode(buttonPin, INPUT);
+}
+
+void loop (){
+  if (digitalRead(buttonPin) == HIGH){
+    myservo.attach(4);
+    pinMode(buttonPin, INPUT);
+  }
+}
+ 
+ if (buttonState != lastButtonState) {
+    if (buttonState == High) {
+      servo = !servo; 
+    }
+  }
+
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  buttonState = digitalRead(buttonPin);
 
 }
